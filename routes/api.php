1f2ff2772a35468a83576daea222c47c9deac7f5
Ola036 <Olaahmed049@gmail.com>
2024-11-25 15:00:13 -0500

    Add CORS middleware to API routes for enhanced security

--- routes/api.php
+++ routes/api.php
@@ -9,29 +9,32 @@ use App\Http\Controllers\User\ChangePasswordController;
 use App\Http\Controllers\Authentication\LoginController;
 use App\Http\Controllers\Authentication\RegisterController;
 use App\Http\Controllers\Authentication\ResetPasswordController;
+use App\Http\Middleware\Cors;
 
-// GUEST
-Route::post('login',       [LoginController::class, 'login']);
-Route::post('register',    [RegisterController::class, 'register']);
-Route::post('forgot',       [ResetPasswordController::class, 'forgot']);
-Route::post('reset',        [ResetPasswordController::class, 'reset']);
+Route::middleware(Cors::class)->group(function () {
+    // GUEST
+    Route::post('login',       [LoginController::class, 'login']);
+    Route::post('register',    [RegisterController::class, 'register']);
+    Route::post('forgot',       [ResetPasswordController::class, 'forgot']);
+    Route::post('reset',        [ResetPasswordController::class, 'reset']);
 
-// USER
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('data',             DataController::class);
+    // USER
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('data',             DataController::class);
 
-    // Authentication
-    Route::post('logout',          [LoginController::class, 'logout']);
+        // Authentication
+        Route::post('logout',          [LoginController::class, 'logout']);
 
-    // Profile && Settings
-    Route::get('profile',          [ProfileController::class, 'show']);
-    Route::put('profile',          [ProfileController::class, 'update']);
-    Route::post('change-password', ChangePasswordController::class);
+        // Profile && Settings
+        Route::get('profile',          [ProfileController::class, 'show']);
+        Route::put('profile',          [ProfileController::class, 'update']);
+        Route::post('change-password', ChangePasswordController::class);
 
-    Route::prefix('2FA')->controller(TwoFAController::class)->group(function () {
-        Route::post('request',      'request');
-        Route::post('enable',       'enable');
-        Route::post('disable',      'disable');
-        Route::post('confirm',      'confirm');
+        Route::prefix('2FA')->controller(TwoFAController::class)->group(function () {
+            Route::post('request',      'request');
+            Route::post('enable',       'enable');
+            Route::post('disable',      'disable');
+            Route::post('confirm',      'confirm');
+        });
     });
 });
